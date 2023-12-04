cd9763098bd2913b52c6e0cf759d45afc7e953ac
varun01patel <varun01patel@gmail.com>
2023-12-05 00:19:38 +0530

    first commit

---
+++ Netflix-gpt/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-lg text-green-400'>hello Netflix-gpt</h1>
+    </>
+  )
+}
+
+export default App
